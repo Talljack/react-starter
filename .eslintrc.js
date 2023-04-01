7697cb574d02bb87f1b12020c68b328dbf0ae455
caoyugang <yugang.cao@tusen.ai>
2023-04-01 12:10:14 +0800

    workflow: update eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
-  extends: ["@antfu", "prettier", "plugin:prettier/recommended"],
+  extends: ['@antfu/eslint-config-react', 'prettier', 'plugin:prettier/recommended'],
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
   },
 };
